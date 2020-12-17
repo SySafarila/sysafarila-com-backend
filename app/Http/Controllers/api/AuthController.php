671a2351fa6849fb8ed50ba1db38999d1ba1e2ce
Syahrul Safarila <sysafarila.official@gmail.com>
2020-12-17 07:17:48 +0700

    Update AuthController.php

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -5,11 +5,26 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'uid' => 'required',
+            'name' => 'required',
+            'email' => 'required|email'
+        ]);
+
+        // return $validator;
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->messages()
+            ], 200);
+        }
+
         if (User::where('email', $request->email)->first()) {
             return response()->json([
                 'message' => 'Failed',
