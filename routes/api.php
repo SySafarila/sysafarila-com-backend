aefdac666334929f0854d16fefadd75dbcb13a9a
Syahrul Safarila <sysafarila.official@gmail.com>
2021-01-01 10:09:30 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,14 +21,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
+    // Post GET|HEAD
     Route::get('/posts', [PostsController::class, 'index'])->name('posts.index');
     Route::get('/posts/sitemap', [PostsController::class, 'sitemap'])->name('posts.sitemap');
     Route::get('/posts/{post}', [PostsController::class, 'show'])->name('posts.show');
 
+    // Post PATCH, POST, DELETE
     Route::post('/posts', [PostsController::class, 'store'])->name('posts.store')->middleware('origin');
     Route::patch('/posts/{post}', [PostsController::class, 'update'])->name('posts.update')->middleware('origin');
     Route::delete('/posts/{post}', [PostsController::class, 'delete'])->name('posts.delete')->middleware('origin');
     Route::post('/auth/reg', [AuthController::class, 'register']);
+
+    // Origin GET|HEAD
     Route::get('/origin', function () {
         return request()->server('HTTP_REFERER');
     });
